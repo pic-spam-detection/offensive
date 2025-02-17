265e1a736a69ecf3886f0e6b7dae55898f60f5f2
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2025-02-17 09:33:27 +0100

    Add cli entrypoint

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import click
+from src.models.llm_based_model import LLM
 
 @click.group()
 def main():
     pass
 
+# TODO support different models
 @main.command()
-def generate():
-    pass
+@click.option('--n_samples', default=1, help='Number of samples to generate.')
+@click.option('--output', default="out.txt", help='Output file path.')
+def generate(n_samples, output):
+    model = LLM()
+
+    emails = []
+
+    for _ in range(n_samples):
+        spam = model.generate()
+        emails.append(spam)
+
+    with open(output, 'w') as file:
+        file.write('\n'.join(emails))
 
 @main.command()
 def evaluate():
-    pass
+    print("evaluate")
