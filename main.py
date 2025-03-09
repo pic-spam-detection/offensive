b7b919a736d549d0a2ce46317e3883d7330cf919
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2025-03-09 17:05:58 +0100

    Add option for selecting a model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+from gpt_model import GPT
 from src.evaluation.evaluate import run_evaluation_suite
 from src.models.llm_based_model import LLM
 from tqdm import tqdm
@@ -9,17 +10,26 @@ def main():
     pass
 
 
-# TODO support different models
 @main.command()
 @click.option("--n_samples", default=1, help="Number of samples to generate.")
+@click.option(
+    "--model",
+    type=click.Choice(["GPT4", "Phi3"], case_sensitive=False),
+    help="Model to use for generating samples.",
+)
 @click.option("--output", default="out.txt", help="Output file path.")
-def generate(n_samples, output):
-    model = LLM()
+def generate(n_samples, model, output):
+    if model.lower() == "GPT4".lower():
+        generator = GPT()
+    elif model.lower() == "Phi3".lower():
+        generator = LLM()
+    else:
+        raise Exception(f"Unknown model: {model}")
 
     emails = []
 
     for _ in tqdm(range(n_samples)):
-        spam = model.generate()
+        spam = generator.generate()
         emails.append(spam)
 
     print(emails)
@@ -30,12 +40,21 @@ def generate(n_samples, output):
     print(f"{output} created or overwritten successfully.")
 
 
-# TODO support different models
 @main.command()
-def evaluate():
-    model = LLM()
+@click.option(
+    "--model",
+    type=click.Choice(["GPT4", "Phi3"], case_sensitive=False),
+    help="Model to use for generating samples.",
+)
+def evaluate(model):
+    if model.lower() == "GPT4".lower():
+        generator = GPT()
+    elif model.lower() == "Phi3".lower():
+        generator = LLM()
+    else:
+        raise Exception(f"Unknown model: {model}")
 
-    run_evaluation_suite(model)
+    run_evaluation_suite(generator)
 
 
 if __name__ == "__main__":
