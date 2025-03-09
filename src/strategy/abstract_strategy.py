341dc6a805148669c4ec636e45d3c87071a8b28f
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2025-03-09 17:39:05 +0100

    Use absolute paths

--- src/strategy/abstract_strategy.py
+++ src/strategy/abstract_strategy.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
-from abstract_model import AbstractModel
-from dataset.dataset import SpamDataset
+from src.models.abstract_model import AbstractModel
+from src.dataset.dataset import SpamDataset
 
 
 class AbstractStrategy(ABC):
