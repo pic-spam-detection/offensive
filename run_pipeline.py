c3f59e33b599fc27bccc440fa0498aa2e9eb0d58
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2025-03-11 15:20:58 +0100

    Add counter for steps in pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -6,8 +6,12 @@ def run_pipeline(config_file):
     with open(config_file, "r") as file:
         config = yaml.safe_load(file)
 
+    n_steps = len(config["pipeline"])
+    i = 0
+
     for step in config["pipeline"]:
-        print(f"Running {step['step']}:")
+        i += 1
+        print(f"Running {step['step']} [{i + 1} / {n_steps}]:")
 
         command = ["python", step["script"]]
         command.extend(step["args"])
